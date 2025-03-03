b79e71abc691718638a95f0e99aafa421ba3ea8c
Petra Ivković <83117719+PetraIvkovic@users.noreply.github.com>
2025-03-03 15:25:21 +0100

    update readme with a description

--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+
+def get_sorted_chars(char_count):
+    result = []
+    for char, count in char_count.items():
+        if char.isalpha():  # abc slova
+            result.append({"char": char, "count": count})
+    result.sort(key=lambda x: x["count"], reverse=True)
+    return result
